9364d62fd103b65c61dd17e0443564f40876927a
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-17 12:11:15 -0400

    Edited App.js and Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
 // Needs to provide developer's name & navigation with titles corresponding to different sections
+
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Alyssa Maples</h1>
+            <Navigation />
+        </header>
+    )
+};
+
+export default Header;
